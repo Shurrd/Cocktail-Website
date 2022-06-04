c1c364fd84ae28d31df277f29aa1ff05aaa399bb
unknown <engraaadeyemi@gmail.com>
2022-06-04 15:54:36 +0100

    cocktail

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, aperiam
+        quis tempora nam sint placeat numquam repellendus rerum quam
+        perspiciatis nostrum totam velit deserunt, accusamus optio ex, iure
+        neque esse eligendi praesentium consequuntur. Nulla odio sequi quae
+        consequatur eveniet saepe!
+      </p>
+    </section>
+  );
+};
+
+export default About;
